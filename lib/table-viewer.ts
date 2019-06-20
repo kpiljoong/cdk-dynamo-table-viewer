926aedaf079efa43005b92e122f0b4d092254206
kpiljoong <kpiljoong@gmail.com>
2019-06-21 01:54:37 +0900

    fix lambda runtime

--- lib/table-viewer.ts
+++ lib/table-viewer.ts
@@ -37,7 +37,7 @@ export class TableViewer extends cdk.Construct {
 
     const handler = new lambda.Function(this, 'Rendered', {
       code: lambda.Code.asset(path.join(__dirname, 'lambda')),
-      runtime: lambda.Runtime.NodeJS810,
+      runtime: lambda.Runtime.Nodejs810,
       handler: 'index.handler',
       environment: {
         TABLE_NAME: props.table.tableName,
